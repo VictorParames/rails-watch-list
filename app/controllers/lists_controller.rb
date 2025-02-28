9ee681a67c392fe5a2084029a7fe8bdd13aea071
Victor Parames <vparames2012@gmail.com>
2025-02-28 16:50:15 -0300

    Middle(1)

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -18,6 +18,11 @@ class ListsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
+    def destroy
+      @list = List.find(params[:id])
+      @list.destroy
+      redirect_to lists_path, notice: "List was successfully deleted."
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     get "up" => "rails/health#show", as: :rails_health_check
-    resources :lists, only: [:index, :show, :new, :create] do
+    resources :lists, only: [:index, :show, :new, :create, :destroy] do
       resources :bookmarks, only: [:new, :create]
     end
     resources :bookmarks, only: [:destroy]
